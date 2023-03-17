a6a0d46cec9dfff6943cd7997fa6c24b79dc9efd
Akshita Jindal <akshita.jindal@sprinklr.com>
2023-03-17 16:05:07 +0530

    lesson-3

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,53 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+// the first argument to getData query is the starting object. Mostly it won't exist for Query types.
+//  but if I create a resolver for a specific object, then it's first argument would be the return value of the query associated with it.
+
+const product = (_, args) => {
+  return Product.findById(args.id)
+    .lean()
+    .exec()
+}
+
+const products = () => {
+  return Product.find({}).exec()
+}
+
+const newProduct = (_, args, ctx) => {
+  return Product.create({ ...args.input, createdBy: ctx.user._id })
+}
+
+const updateProduct = (_, args) => {
+  // specifying new: true means that i want the return value of this function to be the value after update (not before update)
+  return Product.findByIdAndUpdate(args.id, args.input, { new: true })
+    .lean()
+    .exec()
+}
+
+const removeProduct = (_, args) => {
+  return Product.findByIdAndRemove(args.id)
+    .lean()
+    .exec()
+}
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    // getData(_, args, context, info) {},
+    products,
+    product
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product) {
+      return User.findById(product.createdBy)
+        .lean()
+        .exec()
+    }
   }
 }
